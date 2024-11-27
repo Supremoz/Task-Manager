29a256fd9a20e72f86980fc3cb7af8de5877360c
Lindon Matarong <lindon2817@gmail.com>
2024-11-27 23:20:24 +0800

    Start Here

---
+++ Log-in.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task_Manager
+{
+    /// <summary>
+    /// Interaction logic for Log_in.xaml
+    /// </summary>
+    public partial class Log_in : Window
+    {
+        public Log_in()
+        {
+            InitializeComponent();
+        }
+    }
+}
