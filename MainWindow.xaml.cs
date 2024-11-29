370db88e89c115f2fea3bfc54f18e5f9569df33d
krukru12 <radazavonn7@gmail.com>
2024-11-29 18:16:28 +0800

    login&signup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,27 @@ namespace Task_Manager
         {
             InitializeComponent();
         }
+
+        private void btn_signup_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Account Created");
+        }
+
+        private void btn_backtologin_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpView.Visibility = Visibility.Collapsed;
+            LoginView.Visibility = Visibility.Visible;
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Login Successfully");
+        }
+
+        private void btn_createacc_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView.Visibility = Visibility.Collapsed;
+            SignUpView.Visibility = Visibility.Visible;
+        }
     }
 }
